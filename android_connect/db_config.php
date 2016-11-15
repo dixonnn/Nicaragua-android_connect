ef6016fe066383900b74e598bcd18ffa0768794e
Chris Dixon <dixonnn@users.noreply.github.com>
2016-11-15 15:53:22 -0600

    Update db_config.php

--- android_connect/db_config.php
+++ android_connect/db_config.php
@@ -6,7 +6,7 @@ db_config.php
  */
  
 define('DB_USER', "ElBluff1"); // db user
-define('DB_PASSWORD', "Mayn0r"); // db password (mention your db password here)
+define('DB_PASSWORD', ""); // db password (mention your db password here)
 define('DB_DATABASE', "hospital"); // database name
 define('DB_SERVER', "localhost"); // db server
 ?>
