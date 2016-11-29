276febcee1c8530c898768f4f67b95aa21b54ca0
Madeline Lopez <mlopez15@villanova.edu>
2016-11-29 17:52:46 -0500

    Add files via upload

--- fetch_druginfo_row.php
+++ fetch_druginfo_row.php
@@ -1,28 +1,41 @@
 <?php
-
 $drugid = $_GET['drugid'];
+$drugname = $_GET['drugname'];
+$date = $_GET['date'];
 
 $reponse = array();
-
 // connect to db
 require_once('db_config.php');
 
-// Perform sql statement - Hardcoded
-$sql = "SELECT * FROM druginfo WHERE drugid = '000001'";
+if (isset ($date)){
+  // Perform sql statement - Hardcoded
+  if(isset ($drugid)){
+    $sql = "SELECT * FROM shipment WHERE date = '000001' and drugid = '000001'";
+  }
+  else{
+    $sql = "SELECT * FROM shipment WHERE date = '000001' and drugname = 'tylenol'";
+  }
+  // Perform sql statement - user input
+  // $sql = "SELECT * FROM shipment WHERE date = '$date'";
+}
+else{
+  if(isset ($drugid)){
+    $sql = "SELECT * FROM druginfo WHERE drugid = '000001'";
+  }
+  else{
+    $sql = "SELECT * FROM druginfo WHERE drugname = 'tylenol'";
+  }
 
-// Perform sql statement - user input
-// $sql = "SELECT * FROM druginfo WHERE drugid = '$drugid'";
+}
+
 $result = $con->query($sql);
-
 	// Write successful
 	if ($result->num_rows > 0) {
 		
 		$row = $result->fetch_assoc();
-
 		$response["success"] = 1;
 		$response["druginfo"] = array();
 		array_push($response["druginfo"], $row);
-
 		echo json_encode($response);
 	} else {
 		// Write unsuccessful
@@ -30,7 +43,5 @@ $result = $con->query($sql);
 		$repsonse["message"] = "An error occurred";
 		echo json_encode($response);
 	}
-
-
 $con->close();
 ?>
