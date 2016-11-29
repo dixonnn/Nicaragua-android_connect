a258d23fb47f8795b3f7ccdebc57fdbf34ef6702
Chris Dixon <dixonnn@users.noreply.github.com>
2016-11-29 15:41:55 -0500

    Add files via upload

---
+++ add_patientinfo_row.php
@@ -0,0 +1,47 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST') {
+
+	$patname = $_POST['patname'];
+	$patid = $_POST['patid'];
+	$address = $_POST['address'];
+  $telephone = $_POST['telephone'];
+  $gender = $_POST['gender'];
+  $marstat = $_POST['marstat'];
+  $dob = $_POST['dob'];
+  $children = $_POST['children'];
+  $height = $_POST['height'];
+  $weight = $_POST['weight'];
+  $allergies = $_POST['allergies'];
+  $medcond = $_POST['medcond'];
+  $reponse = array();
+
+	// connect to db
+	require_once('db_config.php');
+
+	// Perform sql statement - Hard Coded
+	$sql = "INSERT INTO patientinfo (patname,patid,address,telephone,gender,
+  marstat,dob,children,height,weight,allergies,medcond) VALUES('TestPat1','patid1',
+  '633 Beachomber Lane','7726962517','M','S','03021995','2','100','200','none',
+  'sick')";
+
+	// Perform sql statement - User input
+  //$sql = "INSERT INTO patientinfo (patname,patid,address,telephone,gender,
+  //marstat,dob,children,height,weight,allergies,medcond) VALUES('$patname','$patid',
+  //'$address','$telephone','$gender','$marstat','$dob','$children','$height','$weight','$allergies',
+  //'$medcond')";
+
+	// Write successful
+	if ($con->query($sql) === TRUE) {
+		$response["success"] = 1;
+		$response["message"] = "Record created Successfully";
+		echo json_encode($response);
+	} else {
+		// Write unsuccessful
+		$response["success"] = 0;
+		$repsonse["message"] = "An error occurred";
+		echo json_encode($response);
+	}
+
+mysqli_close($con);
+}
+?>
---
+++ update_patient_info.php
@@ -0,0 +1,65 @@
+<?php
+if($_SERVER['REQUEST_METHOD']=='POST') {
+
+	$patname = $_POST['patname'];
+	$patid = $_POST['patid'];
+	$address = $_POST['address'];
+  $telephone = $_POST['telephone'];
+  $gender = $_POST['gender'];
+  $marstat = $_POST['marstat'];
+  $dob = $_POST['dob'];
+  $children = $_POST['children'];
+  $height = $_POST['height'];
+  $weight = $_POST['weight'];
+  $allergies = $_POST['allergies'];
+  $medcond = $_POST['medcond'];
+  $reponse = array();
+
+	// connect to db
+	require_once('db_config.php');
+
+	// Perform sql statement - Hard Coded
+	$sql = "UPDATE patientinfo
+          SET patname='Chris Dixon'
+              address='641 Conestoga'
+              telephone='7726962517'
+              gender='M'
+              marstat='S'
+              dob='03021995'
+              children='3'
+              height='200'
+              weight='100'
+              allergies='work'
+              medcond='ill'
+          WHERE patid='113649'";
+
+  // Perform sql statement - User Input
+  /*$sql = "UPDATE patientinfo
+          SET patname='$patname'
+              address='$address'
+              telephone='$telephone'
+              gender='$gender'
+              marstat='$marstat'
+              dob='$dob'
+              children='$children'
+              height='$height'
+              weight='$weight'
+              allergies='$allergies'
+              medcond='$medcond'
+          WHERE patid='$patid'"; */
+
+	// Update successful
+	if ($con->query($sql) === TRUE) {
+		$response["success"] = 1;
+		$response["message"] = "Record updated Successfully";
+		echo json_encode($response);
+	} else {
+		// Update unsuccessful
+		$response["success"] = 0;
+		$repsonse["message"] = "An error occurred";
+		echo json_encode($response);
+	}
+
+mysqli_close($con);
+}
+?>
