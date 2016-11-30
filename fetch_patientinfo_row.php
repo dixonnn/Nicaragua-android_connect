671f8332a0ca5b23fad8e9377f0e40c7e1433084
Chris Dixon <dixonnn@users.noreply.github.com>
2016-11-29 20:42:21 -0500

    Add files via upload

--- fetch_patientinfo_row.php
+++ fetch_patientinfo_row.php
@@ -1,20 +1,14 @@
 <?php
 
-//Getting variables
-//$__ is the string name and the ['__'] has temporarily been set
-//as the database value. 
-$patid = $_GET['patid'];
+// $patid = $_GET['patid'];
+$patid = '113649';
 
 $response = array();
 
 //Importing database
 require_once('db_config.php');
 
-//Creating sql query with where clause to get info - hard-coded
-$sql = "SELECT * FROM patientinfo WHERE patid = '113649'";
-
-//Creating sql query with where clause to get info - user input
-//$sql = "SELECT * FROM patientinfo WHERE patID = $patid';
+$sql = "SELECT * FROM patientinfo WHERE patid = '$patid'";
 
 //getting result
 $result = $con->query($sql);
