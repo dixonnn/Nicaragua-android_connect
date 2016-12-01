877c0f3aed1c3931d447cb074141f152e62fa478
Madeline Lopez <mlopez15@villanova.edu>
2016-12-01 17:43:21 -0500

    Add files via upload

--- add_shipment_row.php
+++ add_shipment_row.php
@@ -23,70 +23,65 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
 
 $sql = "SELECT EXISTS(SELECT * FROM druginfo WHERE drugid = '$drugid')";
 $result = $con->query($sql);
+
           // Write successful
-          if (con->query($result)) === TRUE) {
-            $response["success"] = 1;
-            $response["message"] = "Record created Successfully";
-            echo json_encode($response);
-          } else {
+          if ($result) === TRUE) {
+            $response["exist_success"] = 1;
+            $response["exist_message"] = "Record created Successfully";
+
+                            $row = $result->fetch.assoc();
+                            $flag = $row['EXISTS(SELECT * FROM druginfo WHERE drugid = '$drugid')'];
+
+                            if($flag == 1){
+                            $sql =  "UPDATE drugtotal SET drugtotal = drugtotal+$drugtotal";
+                                      // Write successful
+                                      if ($con->query($sql) === TRUE) {
+                                        $response["update_success"] = 1;
+                                        $response["update_message"] = "Record created Successfully";
+                                      } else {
+                                        // Write unsuccessful
+                                        $response["update_success"] = 0;
+                                        $repsonse["update_message"] = "An error occurred";
+                                        echo json_encode($response);
+                                        exit();
+                                      }
+
+                            }else{
+                              $sql = "INSERT INTO druginfo(drugid,drugname,drugtotal) VALUES ('$drugid', '$drugname','$drugtotal') ";
+                                      // Write successful
+                                      if ($con->query($sql) === TRUE) {
+                                        $response["insert_success"] = 1;
+                                        $response["insert_message"] = "Record created Successfully";
+                                      } else {
+                                        // Write unsuccessful
+                                        $response["insert_success"] = 0;
+                                        $repsonse["insert_message"] = "An error occurred";
+                                        echo json_encode($response);
+                                        exit();
+                                      }
+
+                            }
+
+                            $sql = "INSERT INTO shipment(shipdate, drugid, drugname, shipquant) VALUES ('$shipdate','$drugid','$drugname','$drugtotal')";
+                                    // Write successful
+                                    if ($con->query($sql) === TRUE) {
+                                      $response["ship_success"] = 1;
+                                      $response["ship_message"] = "Record created Successfully";
+                                      echo json_encode($response);
+                                    } else {
+                                      // Write unsuccessful
+                                      $response["ship_success"] = 0;
+                                      $repsonse["ship_message"] = "An error occurred";
+                                      echo json_encode($response);
+                                    }
+
+}
+  else {
             // Write unsuccessful
-            $response["success"] = 0;
-            $repsonse["message"] = "An error occurred";
+            $response["exist_success"] = 0;
+            $repsonse["exist_message"] = "An error occurred";
             echo json_encode($response);
           }
           mysqli_close($con);
-          }
-
-$row = $result->fetch.assoc();
-$flag = $row['EXISTS(SELECT * FROM druginfo WHERE drugid = '$drugid')'];
-
-if($flag = 1){
-$sql =  "UPDATE drugtotal SET drugtotal = drugtotal+$drugtotal";
-          // Write successful
-          if ($con->query($sql) === TRUE) {
-            $response["success"] = 1;
-            $response["message"] = "Record created Successfully";
-            echo json_encode($response);
-          } else {
-            // Write unsuccessful
-            $response["success"] = 0;
-            $repsonse["message"] = "An error occurred";
-            echo json_encode($response);
-          }
-          mysqli_close($con);
-          }
 }
-else{
-  $sql = "INSERT INTO druginfo(drugid,drugname,drugtotal) VALUES ('$drugid', '$drugname','$drugtotal') ";
-          // Write successful
-        	if ($con->query($sql) === TRUE) {
-        		$response["success"] = 1;
-        		$response["message"] = "Record created Successfully";
-        		echo json_encode($response);
-        	} else {
-        		// Write unsuccessful
-        		$response["success"] = 0;
-        		$repsonse["message"] = "An error occurred";
-        		echo json_encode($response);
-        	}
-        mysqli_close($con);
-}
-}
-
-
-
-$sql = "INSERT INTO shipment(shipdate, drugid, drugname, shipquant) VALUES ('$shipdate','$drugid','$drugname','$drugtotal')";
-      	// Write successful
-      	if ($con->query($sql) === TRUE) {
-      		$response["success"] = 1;
-      		$response["message"] = "Record created Successfully";
-      		echo json_encode($response);
-      	} else {
-      		// Write unsuccessful
-      		$response["success"] = 0;
-      		$repsonse["message"] = "An error occurred";
-      		echo json_encode($response);
-      	}
-      mysqli_close($con);
-      }
 ?>
