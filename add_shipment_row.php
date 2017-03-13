011e34099753f2d1b1caa2dd25834a4a9144f53d
Chris Dixon <dixonnn@users.noreply.github.com>
2017-03-13 16:31:50 -0600

    Add files via upload

--- add_shipment_row.php
+++ add_shipment_row.php
@@ -2,14 +2,14 @@
 
 if($_SERVER['REQUEST_METHOD']=='POST') {
 	
-	//$drugid = $_POST['drugid'];
-	$drugid = '555545';
-	//$drugname = $_POST['drugname'];
-	$drugname = 'REAL drugs';
+	$drugid = $_POST['drugid'];
+	//$drugid = '555545';
+	$drugname = $_POST['drugname'];
+	//$drugname = 'REAL drugs';
   	//$shipdate = $_POST['shipdate'];
   	$shipdate = '2016-12-01';
-	//$drugincr = $_POST['drugincr'];
-	$drugincr = '500';
+	$drugincr = $_POST['drugincr'];
+	//$drugincr = '500';
 
 	$reponse = array();
 
@@ -20,65 +20,65 @@ if($_SERVER['REQUEST_METHOD']=='POST') {
 	$result = $con->query($sql);
 	
 
-        // Drug EXISTS -> UPDATE druginfo with increment
-        if ($result->num_rows > 0) {
-        	$response["exist_success"] = 1;
-        	$response["exist_message"] = "Record created Successfully";
+    // Drug EXISTS -> UPDATE druginfo with increment
+    if ($result->num_rows > 0) {
+     	$response["exist_success"] = 1;
+       	$response["exist_message"] = "Drug exists already";
 
 		$sql =  "UPDATE druginfo SET drugtotal = drugtotal+$drugincr";
                         
 		// Update successful
-                if ($con->query($sql) === TRUE) {
-	        	$response["update_success"] = 1;
-	                $response["update_message"] = "Record created Successfully";
+        if ($con->query($sql) === TRUE) {
+	       	$response["update_success"] = 1;
+	        $response["update_message"] = "Existing Drug: druginfo update successful";
 			
 		// Update unsuccessful
-                } else { 
-                        $response["update_success"] = 0;
-                        $repsonse["update_message"] = "An error occurred";
-                        echo json_encode($response);
-                        exit();
-                }
+        } else { 
+            $response["update_success"] = 0;
+            $repsonse["update_message"] = "Existing Drug: An error occurred during druginfo update";
+            echo json_encode($response);
+            exit();
+		}
                 	
 
 	// Drug does not EXIST -> INSERT new drug row in druginfo
 	} else {
-        	$response["exist_success"] = 0;
-           	$repsonse["exist_message"] = "Drug doesnt exist -> INSERT new row.";
+       	$response["exist_success"] = 0;
+       	$repsonse["exist_message"] = "Drug doesnt exist -> INSERT new row.";
 		
 		$sql = "INSERT INTO druginfo(drugid,drugname,drugtotal) VALUES ('$drugid', '$drugname','$drugincr') ";
                 
 		// Insert successful
-                if ($con->query($sql) === TRUE) {
-	        	$response["insert_success"] = 1;
-	                $response["insert_message"] = "Record created Successfully";
+        if ($con->query($sql) === TRUE) {
+	       	$response["insert_success"] = 1;
+	        $response["insert_message"] = "New Drug: druginfo insert successful";
 
 		// Insert unsuccessful
-                } else {
-                        $response["insert_success"] = 0;
-                        $repsonse["insert_message"] = "An error occurred";
-                        echo json_encode($response);
-                        exit();
-                }
-	
-            	echo json_encode($response);
+        } else {
+            $response["insert_success"] = 0;
+            $repsonse["insert_message"] = "New Drug: An error occurred during druginfo insert";
+            echo json_encode($response);
+            exit();
         }
+	
+          	echo json_encode($response);
+    }
 	
 	// If the program has gotten this far, it's time to insert a new shipment record
 	$sql = "INSERT INTO shipment(shipdate, drugid, drugname, shipquant) VALUES ('$shipdate','$drugid','$drugname','$drugincr')";
                 
 	// Insert successful
-        if ($con->query($sql) === TRUE) {
+    if ($con->query($sql) === TRUE) {
 		$response["ship_success"] = 1;
-                $response["ship_message"] = "Record created Successfully";
-                echo json_encode($response);
+        $response["ship_message"] = "Shipment Insert: Record created Successfully";
+        echo json_encode($response);
 
 	// Insert unsuccessful
-        } else {
-                $response["ship_success"] = 0;
-                $repsonse["ship_message"] = "An error occurred";
-                echo json_encode($response);
-        }
+    } else {
+        $response["ship_success"] = 0;
+        $repsonse["ship_message"] = "Shipment Insert: An error occurred";
+        echo json_encode($response);
+    }
 
 	mysqli_close($con);
 }
